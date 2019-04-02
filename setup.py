5fe84dd2bf1e3a18f315abbe0f8e90a39959a197
turing-20 <jashanpreetsingh2502@gmail.com>
2019-04-02 15:53:32 +0530

    just ready

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-import setuptools
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-setuptools.setup(
-     name='coding-help',  
-     version='0.1',
-     scripts=['dokr'] ,
-     author="Jashanpreet Singh",
-     author_email="Jashanpreetsingh2502@gmail.com",
-     description="A package to help for learnng coding.",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/turing-20/coding-help",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+from distutils.core import setup
+setup(
+  name = 'coding-help',         
+  packages = ['coding-help'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'some coding help.',   
+  author = 'Jashanpreet Singh',                   
+  author_email = 'Jashanpreetsingh2502@gmail.com',      
+  url = 'https://github.com/turing-20/coding-help',   
+  download_url = 'https://github.com/turing-20/coding-help/archive/v_0.01.tar.gz',    
+  keywords = ['coding', 'help', 'study'],   
+  install_requires=[            
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
